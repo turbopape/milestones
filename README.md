8791913c7bbb525b128ae766c4c7e0a81f95ef90
Rafik NACCACHE <rafik.naccache@gmail.com>
2014-10-11 13:57:36 +0100

    First Commit of automagic.tools Milestones

---
+++ README.md
@@ -0,0 +1,13 @@
+# milestones
+
+A Clojure Library that processes tasks with constraints into automatic schedules.
+
+## Usage
+
+TODO
+
+## License
+
+Copyright © 2014 rafik NACCACHE and Contributors
+
+Distributed under the GNU GPL v3.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject milestones "0.1.0-SNAPSHOT"
+  :description "Milestones : the automagic project Scheduler"
+  :url "http://automagic.tools/milestones"
+  :license {:name "GNU GPL v3.0"
+            :url "https://www.gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
